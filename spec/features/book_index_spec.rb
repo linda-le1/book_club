5799ecf1d5ab8e07f8ff5907c198ed1188fb447a
Linda Le <yueling1222@gmail.com>
2019-12-14 22:38:39 -0700

    Add test file for first story

---
+++ spec/features/book_index_spec.rb
@@ -0,0 +1,39 @@
+
+require "rails_helper"
+
+RSpec.describe "As a visitor", type: :feature do
+  before :each do
+
+  @haruki = Author.create(name: "Haruki Murakami")
+
+  @book_1 = @haruki.books.create(title:       "The Wind-Up Bird Chronicle",
+                       pages:      607,
+                       publication_year:  "1994")
+
+  @book_2 = @haruki.books.create(title:       "Norwegian Wood",
+                       pages:      296,
+                       publication_year:  "1987")
+
+end
+
+  it "can see a list of all books and their attributes" do
+
+
+    visit "/books"
+
+    within "#book-#{@book_1.id}" do
+      expect(page).to have_content(@book_1.title)
+      expect(page).to have_content("By #{@haruki.name}")
+      expect(page).to have_content("Number of Pages: #{@book_1.pages}")
+      expect(page).to have_content("Published in #{@book_1.publication_year}")
+    end
+
+    within "#book-#{@book_2.id}" do
+      expect(page).to have_content(@book_2.title)
+      expect(page).to have_content("By #{@haruki.name}")
+      expect(page).to have_content("Number of Pages: #{@book_2.pages}")
+      expect(page).to have_content("Published in #{@book_2.publication_year}")
+    end
+
+  end
+end
